337ef1bce300af99b94dd8a7287c3c6115f58058
Tiago Almeida <tjsa@ua.pt>
2020-04-05 18:47:20 -0400

    Updated staging deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,39 @@
 module.exports = {
     apps : [{
         name: "expressServer",
-        script: "./expressServer.js"
-    }]
-}
+        script: "./expressServer.js",
+
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        },
+        env_staging: {
+            NODE_ENV: 'staging'
+        }
+    }],
+
+    deploy : {
+        production : {
+            user : 'testSiteApp',
+            host : '104.239.240.55',
+            ref  : 'origin/master',
+            repo : 'git@github.com:Scalabull/get-tested-covid19.git',
+            path : '/home/testSiteApp/get-tested-covid19',
+            'post-deploy' : 'npm run server:build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save'
+        },
+        staging : {
+            user : 'testSiteApp',
+            host : 'localhost',
+            ref  : 'staging',
+            repo : '.',
+            path : '/home/testSiteApp/get-tested-covid19',
+            'post-deploy' : 'npm run server:build && pm2 startOrRestart ecosystem.config.js --env staging && pm2 save'
+        }
+    }
+};
