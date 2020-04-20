048ba91b6e518c6a4347792272fb3f81b61044a4
Kevin Genus <kevin.genus@gmail.com>
2020-04-20 01:21:31 -0400

    Adds work completed by Mars Wang

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -15,110 +15,110 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React from 'react';
 
 // reactstrap components
-import {
-  Card,
-  CardBody
-} from "reactstrap";
+import { Card, CardBody } from 'reactstrap';
 
 class CardList extends React.Component {
-  componentDidMount() {
-  }
-  
-  state = {
+    componentDidMount() {}
 
-  };
+    state = {};
 
-  onExiting = () => {
+    onExiting = () => {};
 
-  };
+    onExited = () => {};
 
-  onExited = () => {
-
-  };
-
-  render() {
-    let topResults = this.props.items.slice(0,9);
-    if (topResults === null || topResults.length === 0){
-      return (
-        <>
-          <Card className="shadow shadow-lg--hover mt-5">
-            <CardBody>
-                <div className="d-flex px-3">
-                <div>
-                    <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                    <i className="ni ni-fat-remove" />
-                    </div>
-                </div>
-                <div className="pl-4">
-                    <h5 className="title text-primary">
-                      No test centers were located within 40 miles of your zip code.
-                    </h5>
-                    <p className="text-muted">
-                      Our data may be incomplete. Please check with your doctor and/or local government, and inform us if we are missing test centers in your area.
-                    </p>
-                    
-                </div>
-                </div>
-            </CardBody>
-        </Card>
-        </>
-      )
-    } else{
-      return (
-        <>
-          {
-            
-            topResults.map(item => {
-              return <Card key={item.name} className="shadow shadow-lg--hover mt-5">
+    render() {
+        let topResults = this.props.items.slice(0, 9);
+        if (topResults === null || topResults.length === 0) {
+            return (
+                <>
+                    <Card className='shadow shadow-lg--hover mt-5'>
                         <CardBody>
-                            <div className="d-flex px-3">
-                            <div>
-                                <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-                                <i className="ni ni-check-bold" />
+                            <div className='d-flex px-3'>
+                                <div>
+                                    <div className='icon icon-shape bg-gradient-warning rounded-circle text-white'>
+                                        <i className='ni ni-fat-remove' />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="pl-4">
-                                <h5 className="title text-success">
-                                {item.name}
-                                </h5>
-                                {item.dist !== null && item.dist !== undefined &&
-                                  <small className="text-black">
-                                    Est. Distance: {item.dist.toFixed(2)} miles
-                                  </small>
-                                }
-                                <p>
-                                    {item.address}, {item.city} {item.state}, {item.zip}
-                                </p>
-                                <h6>
-                                    Doctor screen required before arrival? {item.docScreen}
-                                </h6>
-                                <h6>
-                                    Appointment Required?  {item.appReq}
-                                </h6>
-                                <h6>
-                                    Is this a drive-thru testing site (stay in your car)? {item.driveThru}
-                                </h6>
-                                <h6>
-                                    Is this a walk-in testing site (must come inside the facility at some point)? {item.walkUp}
-                                </h6>
-                                <p>
-                                    <b>Hours:</b> {item.hours}
-                                    <br />
-                                    <b>Phone:</b> {item.phone}
-                                </p>
-                            </div>
+                                <div className='pl-4'>
+                                    <h5 className='title text-primary'>
+                                        No test centers were located within 40
+                                        miles of your zip code.
+                                    </h5>
+                                    <p className='text-muted'>
+                                        Our data may be incomplete. Please check
+                                        with your doctor and/or local
+                                        government, and inform us if we are
+                                        missing test centers in your area.
+                                    </p>
+                                </div>
                             </div>
                         </CardBody>
                     </Card>
-            })
-          }
-        </>
-      );
+                </>
+            );
+        } else {
+            return (
+                <>
+                    {topResults.map((item) => {
+                        return (
+                            <Card
+                                key={item.name}
+                                className='shadow shadow-lg--hover mb-5'
+                            >
+                                <CardBody>
+                                    <div className='d-flex px-3'>
+                                        <div className='pl-4'>
+                                            <h5 className='title text-success'>
+                                                {item.name}
+                                            </h5>
+                                            {item.dist !== null &&
+                                                item.dist !== undefined && (
+                                                    <small className='text-black'>
+                                                        Est. Distance:{' '}
+                                                        {item.dist.toFixed(2)}{' '}
+                                                        miles
+                                                    </small>
+                                                )}
+                                            <p>
+                                                {item.address}, {item.city}{' '}
+                                                {item.state}, {item.zip}
+                                            </p>
+                                            <h6>
+                                                Doctor screen required before
+                                                arrival? {item.docScreen}
+                                            </h6>
+                                            <h6>
+                                                Appointment Required?{' '}
+                                                {item.appReq}
+                                            </h6>
+                                            <h6>
+                                                Is this a drive-thru testing
+                                                site (stay in your car)?{' '}
+                                                {item.driveThru}
+                                            </h6>
+                                            <h6>
+                                                Is this a walk-in testing site
+                                                (must come inside the facility
+                                                at some point)? {item.walkUp}
+                                            </h6>
+                                            <p>
+                                                <b>Hours:</b> {item.hours}
+                                                <br />
+                                                <b>Phone:</b> {item.phone}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        );
+                    })}
+                </>
+            );
+        }
     }
-  }
 }
 
 export default CardList;
--- src/components/TestSiteList.js
+++ src/components/TestSiteList.js
@@ -15,107 +15,99 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import CardList from "components/CardList";
-import zipLookups from "assets/data/zipLookups"
-import haversine from "haversine";
-import qs from "query-string";
+import React from 'react';
+import CardList from 'components/CardList';
+import TestSiteMap from 'components/TestSiteMap';
+import zipLookups from 'assets/data/zipLookups';
+import haversine from 'haversine';
+import qs from 'query-string';
 
 // reactstrap components
-import {
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+import { Row, Col } from 'reactstrap';
 
 // DISTANCE THRESHOLD FOR SEARCH RESULTS (in Miles, Haversine distance)
 const DISTANCE_THRESH = 40;
 
-const setQueryStringWithoutPageReload = qsValue => { 
-    const newurl = window.location.protocol + 
-        window.location.host + 
-        window.location.pathname + 
+const setQueryStringWithoutPageReload = (qsValue) => {
+    const newurl =
+        window.location.protocol +
+        window.location.host +
+        window.location.pathname +
         qsValue;
- 
-    window.history.pushState({ path: newurl }, "", newurl);
+
+    window.history.pushState({ path: newurl }, '', newurl);
 };
 
-const setQueryStringValue = ( 
-    key, 
-    value, 
+const setQueryStringValue = (
+    key,
+    value,
     queryString = window.location.search
- ) => { 
-     const values = qs.parse(queryString); 
-     const newQsValue = qs.stringify({ ...values, [key]: value }); 
-     setQueryStringWithoutPageReload(`?${newQsValue}`);
+) => {
+    const values = qs.parse(queryString);
+    const newQsValue = qs.stringify({ ...values, [key]: value });
+    setQueryStringWithoutPageReload(`?${newQsValue}`);
 };
 
-const getQueryStringValue = ( 
-    key, 
-    queryString = window.location.search
-) => { 
-    const values = qs.parse(queryString); 
+const getQueryStringValue = (key, queryString = window.location.search) => {
+    const values = qs.parse(queryString);
     return values[key];
 };
-
 
 function isNumeric(s) {
     return !isNaN(s - parseFloat(s));
 }
 
 class TestSiteList extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        let zip = getQueryStringValue('zip') || "";
+        let zip = getQueryStringValue('zip') || '';
 
         this.state = {
             initialItems: [],
             items: [],
-            zip: zip
-        }
+            zip: zip,
+        };
 
         this.filterList = this.filterList.bind(this);
     }
 
-    filterList(event){
+    filterList(event) {
         const searchZipStr = event.target.value;
         const zipRE = /^[0-9]{5}$/;
         const zipMatchFlag = zipRE.test(searchZipStr);
-        
-        // Only allow numeric inputs.
-        if(isNumeric(searchZipStr) || searchZipStr === ""){
-            if(zipMatchFlag){
 
+        // Only allow numeric inputs.
+        if (isNumeric(searchZipStr) || searchZipStr === '') {
+            if (zipMatchFlag) {
                 // Zip should be present in lookup table in 99% of cases.
                 let zipLatLng = zipLookups.zipLookups[searchZipStr];
                 let updatedList = this.state.initialItems;
-    
-                if(zipLatLng === undefined){
-                    // If no zip in lookup table, search by exact zip match (not lat, lng distance)   
-                    updatedList = updatedList.filter(function(item){
+
+                if (zipLatLng === undefined) {
+                    // If no zip in lookup table, search by exact zip match (not lat, lng distance)
+                    updatedList = updatedList.filter(function (item) {
                         return item.zip === searchZipStr;
                     });
-                    this.setState({items: updatedList, zip: searchZipStr});
-                } 
-                else{
+                    this.setState({ items: updatedList, zip: searchZipStr });
+                } else {
                     // else zip code is present in lookup table. Use haversine distance w/ lat, lng
                     const start = {
                         latitude: zipLatLng[0],
-                        longitude: zipLatLng[1]
+                        longitude: zipLatLng[1],
                     };
-    
-                    updatedList = updatedList.map(function(item){
+
+                    updatedList = updatedList.map(function (item) {
                         // return any sites within 40 miles.
                         const end = {
                             latitude: item.lat,
-                            longitude: item.lng
+                            longitude: item.lng,
                         };
-    
-                        const dist = haversine(start, end, {unit: 'mile'});
+
+                        const dist = haversine(start, end, { unit: 'mile' });
                         // console.log('calculated distance: ', JSON.stringify(start), JSON.stringify(end), dist);
-                        
-                        let newItem = {...item};
+
+                        let newItem = { ...item };
                         newItem.dist = dist;
                         return newItem;
                     });
@@ -127,67 +119,96 @@ class TestSiteList extends React.Component {
                     updatedList.sort((item1, item2) => {
                         return item1.dist - item2.dist;
                     });
-    
-                    this.setState({items: updatedList, zip: searchZipStr});
+
+                    this.setState({ items: updatedList, zip: searchZipStr });
                 }
-            } 
-            else if (searchZipStr === ''){
-                this.setState({items: this.state.initialItems, zip: searchZipStr});
+            } else if (searchZipStr === '') {
+                this.setState({
+                    items: this.state.initialItems,
+                    zip: searchZipStr,
+                });
             } else {
-                this.setState({zip: searchZipStr});
+                this.setState({ zip: searchZipStr });
             }
         }
-    };
+    }
 
     componentDidMount() {
-        fetch("https://storage.googleapis.com/covid19-resources/testSites.json")
-        .then(res => res.json())
-        .then(
-            (res) => {
-                console.log('external test site directory loaded successfully, CORS success.');
-                this.setState({initialItems: res.testSites, items: res.testSites})
-            },
-            (error) => {
-                console.log(error);
-            }
-        )
-        .then(() => {
-            this.filterList({target: {value: this.state.zip}});
-        })
+        fetch('https://storage.googleapis.com/covid19-resources/testSites.json')
+            .then((res) => res.json())
+            .then(
+                (res) => {
+                    console.log(
+                        'external test site directory loaded successfully, CORS success.'
+                    );
+                    this.setState({
+                        initialItems: res.testSites,
+                        items: res.testSites,
+                    });
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
+            .then(() => {
+                this.filterList({ target: { value: this.state.zip } });
+            });
     }
     render() {
-    
-    return (
-        <Container>
-            <Row className="row-grid align-items-center">
-            
-            <Col className="order-lg-1" lg="10">
-                <div className="d-flex px-3">
-                <div>
-                    <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
-                    <i className="ni ni-building text-primary" />
-                    </div>
-                </div>
-                <div className="pl-6">
-                    <h4 className="display-3 text-white">COVID-19 Testing Centers</h4>
-                    <p className="lead text-white">
-                        We are working around-the-clock to provide accurate information.
-                    </p>
-                </div>
-                </div>
-
-                <form>
-                    <fieldset className="form-group">
-                    <input type="text" maxLength="5" title="Five digit zip code" placeholder="5 digit zip code" className="form-control form-control-lg" onChange={this.filterList} value={this.state.zip}/>
-                    </fieldset>
-                </form>
-
-                <CardList items={this.state.items}/>
-            </Col>
-            </Row>
-        </Container>
-    );
-  }
+        return (
+            <div>
+                <Row
+                    className='row-grid align-items-start pt-6 pb-6
+                            pl-6 pr-6 search-header'
+                >
+                    <Col className='order-lg-1' lg='8'>
+                        <Row>
+                            <h4 className='display-3 text-white'>
+                                Find a COVID-19 community-test center near you.
+                            </h4>
+                        </Row>
+                        <Row>
+                            <p className='text-white'>
+                                We have created a database of community centers
+                                across the U.S. Please enter your location zip
+                                code to find one near you
+                            </p>
+                        </Row>
+                        <Row>
+                            <form>
+                                <fieldset className='form-group'>
+                                    <input
+                                        type='text'
+                                        maxLength='5'
+                                        title='Enter Zip Code (5 digit)'
+                                        placeholder='Enter Zip Code (5 digit)'
+                                        className='form-control form-control-lg'
+                                        onChange={this.filterList}
+                                        value={this.state.zip}
+                                    />
+                                </fieldset>
+                            </form>
+                        </Row>
+                    </Col>
+                    <Col className='order-lg-1' lg='4'></Col>
+                </Row>
+                <Row className='row-grid align-items-start card-list'>
+                    <Row className='pl-5 pr-5'>
+                        <p>
+                            Currently, there are over 200 community testing
+                            centers across U.S.
+                        </p>
+                    </Row>
+                    <Col className='order-lg-1' lg='8'>
+                        <CardList items={this.state.items} />
+                    </Col>
+                    <Col className='order-lg-1' lg='4'>
+                        <TestSiteMap items={this.state.items} />
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default TestSiteList;
