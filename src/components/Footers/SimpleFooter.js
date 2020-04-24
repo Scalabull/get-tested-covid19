b4a417ab6e2d64ceb66573aaa164a1b15aa4edcc
Zachary Boldyga <zach@scalabull.com>
2020-04-24 01:49:16 -0400

    extensive cleanup, removed unused code.

--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -1,35 +1,16 @@
-/*!
 
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 /*eslint-disable*/
 import React from "react";
 import { Link } from "react-router-dom";
 
-// reactstrap components
 import {
-  Button,
   NavItem,
   NavLink,
   Nav,
   NavbarBrand,
   Container,
   Row,
-  Col,
-  UncontrolledTooltip
+  Col
 } from "reactstrap";
 
 class SimpleFooter extends React.Component {
--- src/components/Forms/HomeZipForm.js
+++ src/components/Forms/HomeZipForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
 
-// reactstrap components
 import {
     Button,
     Form,
