87eed586711a9a7752c48a80ddefd8c55321dd73
Joy Wang <w.joy5015@gmail.com>
2020-04-28 23:21:20 -0400

    chore: issue #175; plug in server API to frontend

--- src/components/TestSiteList.js
+++ src/components/TestSiteList.js
@@ -3,45 +3,17 @@ import React from 'react';
 import styled from 'styled-components'
 import CardList from 'components/CardList';
 import TestSiteMap from 'components/TestSiteMap';
-import haversine from 'haversine';
 import qs from 'query-string';
 import HomeZipForm from 'components/Forms/HomeZipForm.js';
+import axios from "axios";
 
 import { Row, Col } from 'reactstrap';
 import hero1 from '../assets/img/hero/Hero1.png';
 
 import { GoogleApiWrapper } from 'google-maps-react';
 
-// DISTANCE THRESHOLD FOR SEARCH RESULTS (in Miles, Haversine distance)
-const DISTANCE_THRESH = 40;
-
-// Check if GeocoderResult object return form Google Geolocation is a US address.
-function isUSLocation(geocoderResult){
-    let filtered = geocoderResult.address_components.filter((component) => {
-        return component.types &&
-           component.types.includes("country") &&
-           component.short_name === "US";
-    });
-
-    return filtered.length > 0;
-}
-
-function returnPostalCode(geocoderResult){
-    let zipCode = null;
-
-    geocoderResult.address_components.forEach(component => {
-        if(component.types &&
-            component.types.includes("postal_code") &&
-            component.short_name.length === 5){
-
-            zipCode = component.short_name;
-        }
-    });
-
-    return zipCode;
-}
-
-// If Geolocation runs successfully and points to a US address, return lat, lng
+// This function is for the "Use my location" feature. If Geolocation runs successfully and points to a US address, return lat, lng
+/*
 function tryGeolocation(geocoder, callback){
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -49,7 +21,6 @@ function tryGeolocation(geocoder, callback){
             lat: position.coords.latitude,
             lng: position.coords.longitude
           };
-
           console.log('location found: ', pos);
           geocoder.geocode({'location': pos}, (res, status) => {
             if (status === 'OK') {
@@ -76,17 +47,7 @@ function tryGeolocation(geocoder, callback){
           callback(new Error('Geolocation not supported.'));
       }
 }
-
-function codeAddress(zip, geocoder, callback) {
-    geocoder.geocode( { 'address': zip}, function(results, status) {
-        if (status === 'OK') {
-            callback(null, results[0].geometry.location);
-        }
-        else{
-            callback(status, null);
-        }
-    });
-}
+*/
 
 const getQueryStringValue = (key, queryString = window.location.search) => {
     const values = qs.parse(queryString);
@@ -110,101 +71,21 @@ class TestSiteList extends React.Component {
             searchZip: zip,
             defaultZip: zip || '10001',
             zipLatLng: null,
-            isFetching: true,
+            // isFetching: true,
         };
 
-        this.filterList = this.filterList.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onSubmit(zipStr) {
-        this.filterList(zipStr + "");
-    }
-
-    filterList(searchZipStr) {
+    onSubmit = async (zipStr) => {
         const zipRE = /^[0-9]{5}$/;
-        const zipMatchFlag = zipRE.test(searchZipStr);
-
-        // Only allow numeric inputs.
-        if (isNumeric(searchZipStr) || searchZipStr === '') {
+        const zipMatchFlag = zipRE.test(zipStr); // returns true if zip code is valid 5 digit number
+        if (isNumeric(zipStr) || zipStr === '') {
             if (zipMatchFlag) {
-                let updatedList = this.state.initialItems;
-
-                let geocoder = new this.props.google.maps.Geocoder();
-                codeAddress(searchZipStr, geocoder, (err, googleLatLng) => {
-                    if(!err){
-                        //Use haversine distance w/ lat, lng
-                        const zipLatLng = {
-                            latitude: googleLatLng.lat(),
-                            longitude: googleLatLng.lng(),
-                        };
-
-                        updatedList = updatedList.map(function (item) {
-                            // return any sites within 40 miles.
-                            const end = {
-                                latitude: item.lat,
-                                longitude: item.lng,
-                            };
-
-                            const dist = haversine(zipLatLng, end, { unit: 'mile' });
-                            let newItem = { ...item };
-                            newItem.dist = dist;
-                            return newItem;
-                        });
-
-                        updatedList = updatedList.filter((item) => {
-                            return item.dist < DISTANCE_THRESH;
-                        });
-
-                        updatedList.sort((item1, item2) => {
-                            return item1.dist - item2.dist;
-                        });
-
-                        this.setState({ items: updatedList, zipLatLng, searchZip: searchZipStr });
-                    }
-                });
-            } else {
-                this.setState({
-                    items: [],
-                });
+                const centersInRange = await axios.get(`/api/test-centers/${zipStr}`)
+                this.setState({ items: centersInRange });
             }
         }
-    }
-
-    componentDidMount() {
-        fetch('https://storage.googleapis.com/covid19-resources/testSites.json')
-            .then((res) => res.json())
-            .then(
-                (res) => {
-                    this.setState({
-                        initialItems: res.testSites,
-                        items: res.testSites,
-                    });
-                },
-                (error) => {
-                    console.log(error);
-                }
-            )
-            .then(() => {
-                this.setState({
-                    isFetching: false,
-                });
-
-                // try to get previously approved location
-                const zipQueryString = getQueryStringValue('zip');
-                if (!zipQueryString && navigator && navigator.permissions) {
-                    navigator.permissions.query({ name: 'geolocation' })
-                        .then(status => {
-                            if (status && status.state === 'granted') {
-                                this.locateUser();
-                            } else {
-                                this.setDefaultZip();
-                            }
-                        })
-                } else {
-                    this.setDefaultZip();
-                }
-            })
     }
 
     setDefaultZip(searchZip = this.state.defaultZip) {
@@ -213,6 +94,7 @@ class TestSiteList extends React.Component {
         });
     }
 
+    /*
     locateUser(scrollTo = false) {
         let geocoder = new this.props.google.maps.Geocoder();
         tryGeolocation(geocoder, (err, pos, postalCode) => {
@@ -226,8 +108,12 @@ class TestSiteList extends React.Component {
             }
         });
     }
+    */
 
     renderList() {
+
+        //I don't think this is necessary anymore since we're not preloading the database as a large json obj
+        /*
         if (this.state.isFetching) {
             return (
                 <Col className='order-lg-1 text-center mb-9 mt-9 display-2' lg='12'>
@@ -237,6 +123,7 @@ class TestSiteList extends React.Component {
                 </Col>
             );
         }
+        */
 
         const viewItems = this.state.items.slice(0, 10);
 
@@ -283,9 +170,9 @@ class TestSiteList extends React.Component {
                             <HomeZipForm onSubmit={this.onSubmit} searchZip={this.state.searchZip}></HomeZipForm>
                         </Row>
                         <Row>
-                            <div type="button" className='text-white mt-2' onClick={() => this.locateUser(true)}>
+                            {/* <div type="button" className='text-white mt-2' onClick={() => this.locateUser(true)}>
                                 Use my current location
-                            </div>
+                            </div> */}
                         </Row>
                     </Col>
                     <Col className='order-lg-1 mt--100' lg='5'>
@@ -306,7 +193,7 @@ export const RotateAnimation = styled.div`
 -webkit-animation:spin 2s linear infinite;
 -moz-animation:spin 2s linear infinite;
 animation:spin 2s linear infinite;
-    
+
 @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
 @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
 @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
