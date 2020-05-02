ffe8b52c2082f83c59621b7507df2e98c8cb46e5
Jono Lee <jobolee@gmail.com>
2020-05-02 15:46:44 -0700

    Creates new styled-components base infrastructure on index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-/*!
 
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
+import 'normalize.css/normalize.css';
+
+
+// To delete
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
@@ -29,33 +18,52 @@ import Resources from "views/Resources.js";
 import WhenShouldYouGetTested from "views/WhenShouldYouGetTested.js";
 import HowTestingWorks from './views/HowTestWorks/index.js';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact render={props => <TestSiteSearch {...props} />} />
-      <Route
-        path="/resources"
-        exact
-        render={props => <Resources {...props} />}
-      />
-      <Route
-        path="/when-should-you-get-tested"
-        exact
-        render={props => <WhenShouldYouGetTested {...props} />}
-      />
-      <Route
-        path="/how-testing-works"
-        exact
-        render={(props) => <HowTestingWorks {...props} />}
-      />
+// Colors
+const colors = {
+  colorRed: 'red'
+}
 
-      <Route
-        path="/about"
-        exact
-        render={props => <About {...props} />}
-      />
-      <Redirect to="/" />
-    </Switch>
-  </BrowserRouter>,
+// Shared CSS theme variables
+const theme = {
+  ...colors,
+  colorPrimary: colors.colorRed,
+}
+
+// For CSS styling shared across different components
+const GlobalStyle = createGlobalStyle`
+  background-color: ${props => props.theme.colorPrimary};
+`
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={props => <TestSiteSearch {...props} />} />
+        <Route
+          path="/resources"
+          exact
+          render={props => <Resources {...props} />}
+        />
+        <Route
+          path="/when-should-you-get-tested"
+          exact
+          render={props => <WhenShouldYouGetTested {...props} />}
+        />
+        <Route
+          path="/how-testing-works"
+          exact
+          render={(props) => <HowTestingWorks {...props} />}
+        />
+
+        <Route
+          path="/about"
+          exact
+          render={props => <About {...props} />}
+        />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById("root")
 );
