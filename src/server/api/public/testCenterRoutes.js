6ce88b31f0ec0f0a7c1871c0e976702769dbeaeb
Zachary Boldyga <zach@scalabull.com>
2020-05-03 01:57:47 -0400

    added direct lookup by lat,lng

--- src/server/api/public/testCenterRoutes.js
+++ src/server/api/public/testCenterRoutes.js
@@ -55,32 +55,28 @@ router.get('/zip/:zipStr', async (req, res)=>{
     }
 
     const {latitude, longitude} = await zipToLatLng(zipStr);
-    const location = sequelize.literal(`ST_GeomFromText('POINT(${longitude} ${latitude})', 4326)`);
-    const distance = sequelize.fn('ST_DistanceSphere', sequelize.col('geolocation'), location)
+    let testCenters = await performLatLngLookup(latitude, longitude);
 
-    let testCenters = await db.TestCenter.findAll({
-      attributes: {
-        include: [
-          [distance, 'distance']
-        ],
-        exclude: [
-          "id", 
-          "public", 
-          "geolocation",
-          "hours_of_operation",
-          "days_of_operation",
-          "operation_period",
-          "estimated_daily_test_capacity",
-          "comments",
-          "address_freetext_blob",
-          "createdAt",
-          "updatedAt"
-        ]
-      },
-      order: distance,
-      where: sequelize.where(distance, { [Op.lt]: SEARCH_RADIUS }),
-      limit: 250
-    });
+    res.status(200).json({testCenters});
+  } catch (error) {
+    console.error('zip query error: ', error);
+    res.status(500).send()
+  }
+})
+
+router.get('/searchByLatLng', async (req, res)=>{
+  try {
+
+    const { latitude, longitude } = req.query;
+    const parsedLat = Number.parseFloat(latitude);
+    const parsedLng = Number.parseFloat(longitude);
+    if (Number.isNaN(parsedLat) || Number.isNaN(parsedLng) 
+    || parsedLng < -180 || parsedLng > 180 || parsedLat > 90 || parsedLat < -90) { 
+      console.error('Bad user input: ', {latitude, longitude});
+      return res.status(400).send()
+    }
+
+    let testCenters = await performLatLngLookup(latitude, longitude);
 
     res.status(200).json({testCenters});
   } catch (error) {
@@ -104,5 +100,36 @@ router.get('/:id', async (req, res) => {
     res.status(500).send()
   }
 })
+
+async function performLatLngLookup(latitude, longitude) {
+  const location = sequelize.literal(`ST_GeomFromText('POINT(${longitude} ${latitude})', 4326)`);
+  const distance = sequelize.fn('ST_DistanceSphere', sequelize.col('geolocation'), location)
+
+  let testCenters = await db.TestCenter.findAll({
+    attributes: {
+      include: [
+        [distance, 'distance']
+      ],
+      exclude: [
+        "id", 
+        "public", 
+        "geolocation",
+        "hours_of_operation",
+        "days_of_operation",
+        "operation_period",
+        "estimated_daily_test_capacity",
+        "comments",
+        "address_freetext_blob",
+        "createdAt",
+        "updatedAt"
+      ]
+    },
+    order: distance,
+    where: sequelize.where(distance, { [Op.lt]: SEARCH_RADIUS }),
+    limit: 250
+  });
+
+  return testCenters;
+}
 
 module.exports = router
