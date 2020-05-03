ae2c850c5be2d6e3ad2a053f7176a56d9600c4fa
Zachary Boldyga <zach@scalabull.com>
2020-05-03 02:12:05 -0400

    google geocoding key moved to env var.

--- src/server/api/public/utils/zipToLatLng.js
+++ src/server/api/public/utils/zipToLatLng.js
@@ -1,5 +1,8 @@
 const zipLookup = require('../../../data/zipLookups.js');
 const axios = require('axios');
+const GOOGLE_GEOCODING_KEY = process.env.GOOGLE_GEOCODING_KEY;
+
+console.log('key is: ', GOOGLE_GEOCODING_KEY);
 
 async function zipToLatLng(zip) {
     const cacheLookup = zipLookup.zipLookups[zip + ""];
@@ -9,7 +12,7 @@ async function zipToLatLng(zip) {
         return latLng;
     }
 
-    const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address="+zip+'&key=AIzaSyCj5wGAsi1ppD8qf6Yi-e6fMChdck7BMVg')
+    const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address="+zip+'&key=' + GOOGLE_GEOCODING_KEY)
 
     const latitude = response.data.results[0].geometry.location.lat;
     const longitude = response.data.results[0].geometry.location.lng;
