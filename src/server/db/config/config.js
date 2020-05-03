07872e73920308d541dcbb1a8088dcec18b83b7a
Zachary Boldyga <zach@scalabull.com>
2020-05-02 22:08:48 -0400

    test center query by zip has basic functionality. WIP: optimization, security, linking client-side query.

--- src/server/db/config/config.js
+++ src/server/db/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_development`,
+    database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
     operatorsAliases: 0,
