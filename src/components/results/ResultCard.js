546950a7906bc4780dbac73dcf011715cd201d3c
Jono Lee <jobolee@gmail.com>
2020-05-03 16:43:35 -0700

    Adds in walk up and drive through facilities to ResultCard

--- src/components/results/ResultCard.js
+++ src/components/results/ResultCard.js
@@ -1,55 +1,85 @@
 
 import React from 'react';
-import icon1 from '../../assets/img/icons/card/Hospital.png';
-import icon2 from '../../assets/img/icons/card/Doc-blue.png';
-import icon3 from '../../assets/img/icons/card/DriveThrough.png';
-import icon4 from '../../assets/img/icons/card/Appointment-blue.png';
+import iconInFacility from '../../assets/img/icons/card/Hospital.png';
+import iconDrive from '../../assets/img/icons/card/DriveThrough.png';
 import styled from 'styled-components';
 import ReadMore from '../shared/ReadMore.js';
+import { ButtonGroup } from 'reactstrap';
 
 const StyledResultCard = styled.div`
   border-top: 1px solid #eee;
-  padding: 15px;
+  padding: 20px;
 
   h3 {
     font-size: 1.5rem;
   }
 
-  .card__call {
-    @media screen and (min-width: ${props => props.theme.bpSmall}) {
-      display: none;
-    }
-  }
-
   p {
     margin-bottom: 5px;
-    font-size: 15px;
+    font-size: 14px;
+  }
+
+  .card__req {
+    margin: -3px 0 5px;
+  }
+
+  .card__actions {
+    margin-top: 15px;
+  }
+
+  .card__abilities {
+    margin-top: 10px;
+    font-weight: 500;
+  }
+
+  .card__ability-icon {
+    height: 20px;
+    margin-right: 5px;
   }
 `
 
 class ResultCard extends React.Component {
   render() {
-    const { name, address, city, state, zip, phone, link, description } = this.props.item;
+    const { name, address, city, state, zip, phone, link, description, docScreen, appReq, driveThru, walkUp } = this.props.item;
     return (
         <StyledResultCard>
           <h3>{this.props.index + 1}. {name}</h3>
-          <p className="card__location text-secondary">
-            {address}, {city} &middot; <a href={`https://www.google.com/maps/dir/current+location/${address}+${city}+${state}+${zip}/`} target="_blank" rel="noopener noreferrer">Get directions</a>
-          </p>
-          <p className="card__contact text-secondary">
-            {phone !== '' && (
-              <>
-                {phone.split(')')[0]}) {phone.split(')')[1]}<span className="card__call"> &middot; <a href={`tel: ${phone}`} target="_blank" rel="noopener noreferrer">Call</a></span>
-              </>
+          <div className="card__req">
+            {docScreen === "No" && appReq === "No" && <span class="badge badge-success">No requirements for testing</span>}
+            {(docScreen !== "No" || appReq !== "No") && (
+              <span class="badge badge-danger">
+                {(docScreen === "Yes" && appReq === "No") && "Doctor's screening "}
+                {(docScreen === "Yes" && appReq === "Yes") && "Doctor's screening and appointment "}
+                {(docScreen === "No" && appReq === "Yes") && "Appointment "}
+                required
+              </span>
             )}
-            {phone !== '' && link !== '' && <> &middot; </>}
-            {link !== '' && <a href={link} target="_blank" rel="noopener noreferrer">{domain_from_url(link)}</a>}
+          </div>
+          <p className="card__meta text-secondary">
+            {address}, {city} &middot; {phone.split(')')[0]}) {phone.split(')')[1]}
           </p>
           <p className="card__descr">
             <ReadMore lines={2}>
                 {description}
             </ReadMore>
           </p>
+          <p className="card__abilities">
+            {walkUp === 'Yes' && (
+              <span><img className="card__ability-icon" src={iconInFacility} alt="Has in-facility testing"/>In-facility testing</span>
+            )}
+            {driveThru === 'Yes' && (
+              <span><img className="card__ability-icon" src={iconDrive} alt="Has drive through testing" />Drive through testing</span>
+            )}
+          </p>
+          <div className="card__actions">
+            <ButtonGroup size="sm">
+              <a class="btn btn-outline-primary" href={`https://www.google.com/maps/dir/current+location/${address}+${city}+${state}+${zip}/`} target="_blank" rel="noopener noreferrer">Get directions</a>
+              {phone !== '' && (
+                <a className="btn btn-outline-primary" href={`tel: ${phone}`}  target="_blank" rel="noopener noreferrer">Call</a>
+              )}
+              {link !== '' && <a className="btn btn-outline-primary" href={link} target="_blank" rel="noopener noreferrer">Visit website</a>}
+            </ButtonGroup>
+          </div>
         </StyledResultCard>
     )
   }
