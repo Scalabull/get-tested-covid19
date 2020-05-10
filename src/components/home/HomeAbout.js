fc6a506e7507668d26671fc65f3c9f71ea9bd292
Jono Lee <jobolee@gmail.com>
2020-05-09 17:50:32 -0700

    Adds image and link to about in HomeAbout section

--- src/components/home/HomeAbout.js
+++ src/components/home/HomeAbout.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import styled from 'styled-components'
+import footerHero from '../../assets/img/hero/Email_illustration.png';
+import { Link } from 'react-router-dom';
 
 const StyledHomeAbout = styled.div`
   border-top: 1px solid #eee;
@@ -8,19 +10,31 @@ const StyledHomeAbout = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  height: 400px;
-  text-align: center;
 
-  h1 {
+  .home-about__content {
+    p {
+      max-width: 600px;
       margin-bottom: 15px;
+    }
   }
 
-  p {
-      margin-bottom: 20px;
-  }
+  @media screen and (max-width: ${props => props.theme.bpSmall}) {
 
-  .container {
-    max-width: 500px;
+    .container {
+      padding: 60px 15px;
+    }
+
+    .home-about__content {
+      text-align: center;
+
+      h2 {
+        font-size: 1.75rem;
+      }
+    }
+
+    .home-about__img {
+      display: none;
+    }
   }
 `
 
@@ -28,10 +42,18 @@ class HomeAbout extends React.Component {
   render() {
     return (
       <StyledHomeAbout>
-        <div className="container">
-          <h2>Helping beat COVID-19</h2>
-          <p>We're a team of volunteers working around-the-clock to provide accurate information on test centers. See an issue? Let us know and we'll fix it. Stay safe.</p>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdr_SEZYn9s6SOahSEGMkrWn4_p-9sCJQf9HtWFyNR9mAPCow/viewform?usp=sf_link" className="btn btn-primary">Contact us</a>
+        <div className="container d-flex align-items-center justify-content-between">
+          <div className="home-about__content">
+            <h2>Helping beat COVID-19</h2>
+            <p>Testing is key to overcoming COVID-19. We're a team of volunteers working around-the-clock to provide accurate information on test centers. See an issue? Let us know and we'll fix it. Stay safe.</p>
+            <div>
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSdr_SEZYn9s6SOahSEGMkrWn4_p-9sCJQf9HtWFyNR9mAPCow/viewform?usp=sf_link" className="btn btn-primary">Contact us</a>
+              <Link className="btn btn-outline-primary ml-2" to="/about">Learn more</Link>
+            </div>
+          </div>
+          <div className="home-about__img">
+            <img src={footerHero} alt='Contact Get Tested COVID-19' />
+          </div>
         </div>
       </StyledHomeAbout>
     );
