8adfe2dbae9e65e233429b9947526344e3bd1a20
unknown <agrawal.suman7@gmail.com>
2020-06-06 12:30:36 -0400

    adding scraper for all states

--- src/server/data/scraper_v2.py
+++ src/server/data/scraper_v2.py
@@ -9,47 +9,39 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import pandas as pd
+data = []
 load_dotenv(os.getenv('ENV'))
 def load_page(self):
     self.driver.get(self.url)
-def page_is_loaded(browser):
-    return browser.find_element_by_id("state") != None
+STATE_LIST =['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 browser = webdriver.Chrome(os.getenv('CHROME_DRIVER'))
-def page_is_loaded1(browser):
-    return browser.find_element_by_id("county") != None
 elem= browser.get(os.getenv('URL'))
-browser.implicitly_wait(20)
-wait=ui.WebDriverWait(browser,20)
-wait.until(page_is_loaded)
 browser.maximize_window()
-browser.find_element_by_xpath("//span[@id='select2-state-container']").click()
-browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(os.getenv('STATE'))
-browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(Keys.ENTER)
-browser.implicitly_wait(20)
-wait=ui.WebDriverWait(browser,20)
-wait.until(page_is_loaded1)
-browser.find_element_by_xpath("//span[@id='select2-county-container']").click()
-browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys("All")
-browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(Keys.ENTER)
-
-result_div=browser.find_elements_by_class_name('result_box')
-links = []
-address = []
-data = []
-name = []
-phone = []
-div =[]
-description =[]
-for r in result_div:
-    try:
-        address=r.text
-        if address != '' : 
-            data.append({"full_address":address})     
-    except:
-        continue
-
+for i in STATE_LIST:
+    def page_is_loaded(browser):
+        return browser.find_element_by_id("state") != None
+    wait=ui.WebDriverWait(browser,20)
+    wait.until(page_is_loaded)
+    browser.find_element_by_xpath("//span[@id='select2-state-container']").click()
+    browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(i)
+    browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 30, poll_frequency=5, ignored_exceptions=['ElementNotVisibleException','ElementNotSelectableException','NoSuchElementException'])
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@id='select2-county-container']")))
+    browser.find_element_by_xpath("//span[@id='select2-county-container']").click()
+    browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys("All")
+    browser.find_element_by_xpath("//input[@class='select2-search__field']").send_keys(Keys.ENTER)
+    result_div=browser.find_elements_by_class_name('result_box')
+    for r in result_div:
+        try:
+            address=r.text
+            if address != '' : 
+                data.append({"full_address":address})    
+        except:
+            continue
+    
 df=pd.DataFrame(data)
 print(df)
 df.to_csv('data.csv')
 browser.quit()
+
 
