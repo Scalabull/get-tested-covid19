fc8914eb1a85fafe8b7ef37b69a45b69aabd83fb
Zach Boldyga <zboldyga@Gmail.com>
2020-06-10 16:01:33 -0400

    added path for alternate csv parse logic, updated test cases.

--- src/data_pipeline/helpers/test_center_csv.py
+++ src/data_pipeline/helpers/test_center_csv.py
@@ -1,28 +1,58 @@
 import csv
 
-def load_valid_csv_rows(csv_file):
+def load_valid_csv_rows(csv_file, is_preprocessed = False):
     valid_test_centers = []
 
     with open(csv_file) as test_centers_file:
-        # Confirm columns are correct, header present
         test_center_reader = csv.reader(test_centers_file)
 
         header = next(test_center_reader)
-        check_if_valid_header_row(header)
+        valid_test_centers = []
 
-        for test_center in test_center_reader:
-            valid_test_center = extract_test_center_from_csv(test_center)
-            if(valid_test_center != None):
-                valid_test_centers.append(valid_test_center)
-            
+        if is_preprocessed:
+            check_if_valid_preprocessed_header_row(header)
+            valid_test_centers = extract_valid_rows(test_center_reader, extract_preprocessed_test_center_row) 
+        else:
+            check_if_valid_header_row(header)
+            valid_test_centers = extract_valid_rows(test_center_reader, extract_test_center_row)
+        
         return valid_test_centers
+        
+
+def extract_valid_rows(test_center_reader, row_extractor):
+    valid_test_centers = []
+    
+    for test_center in test_center_reader:
+        valid_test_center = row_extractor(test_center)
+        if(valid_test_center != None):
+            valid_test_centers.append(valid_test_center)
+        
+    return valid_test_centers
 
 def check_if_valid_header_row(header):
     if(header[0] != 'NAME' or header[1] != 'FULL_ADDRESS' or header[2] != 'PHONE' or header[3] != 'URL' or header[4] != 'DESCRIPTION'):
         raise Exception('CSV_HEADER_FORMAT_INVALID', 'Proper CSV header (and format): NAME,FULL_ADDRESS,PHONE,URL,DESCRIPTION')
 
 # Expects a list of strings: name, full address, phone, URL, description
-def extract_test_center_from_csv(csv_row):
+def extract_test_center_row(csv_row):
+    if(len(csv_row) != 5):
+        return None
+
+    if(csv_row[0] == '' or csv_row[1] == '' or csv_row[2] == ''):
+        return None
+        
+    test_center = {
+        'name': csv_row[0],
+        'full_address': csv_row[1],
+        'phone': csv_row[2],
+        'url': csv_row[3],
+        'description': csv_row[4]
+    }
+
+    return test_center
+
+# TODO: configure properly
+def extract_preprocessed_test_center_row(csv_row):
     if(len(csv_row) != 5):
         return None
 
--- src/data_pipeline/tests/test_center_csv.py
+++ src/data_pipeline/tests/test_center_csv.py
@@ -7,12 +7,12 @@ DATA_PATH = os.path.join(THIS_DIR, os.pardir, 'data')
 class TestCSVMethods(unittest.TestCase):
 
     def test_csv_row_extraction_insufficient_cols(self):
-        self.assertEqual(test_center_csv.extract_test_center_from_csv(['one', 'two', 'three', 'four']), None)
+        self.assertEqual(test_center_csv.extract_test_center_row(['one', 'two', 'three', 'four']), None)
     
     def test_csv_row_extraction_empty_fields(self):
-        self.assertEqual(test_center_csv.extract_test_center_from_csv(['', 'two', 'three', 'four', 'five']), None)
-        self.assertEqual(test_center_csv.extract_test_center_from_csv(['one', '', 'three', 'four', 'five']), None)
-        self.assertEqual(test_center_csv.extract_test_center_from_csv(['one', 'two', '', 'four', 'five']), None)
+        self.assertEqual(test_center_csv.extract_test_center_row(['', 'two', 'three', 'four', 'five']), None)
+        self.assertEqual(test_center_csv.extract_test_center_row(['one', '', 'three', 'four', 'five']), None)
+        self.assertEqual(test_center_csv.extract_test_center_row(['one', 'two', '', 'four', 'five']), None)
 
     def test_csv_row_extraction_basic_case(self):
         target_extract = {
@@ -22,7 +22,7 @@ class TestCSVMethods(unittest.TestCase):
             'url': 'four',
             'description': 'five'
         }
-        self.assertEqual(test_center_csv.extract_test_center_from_csv(['one', 'two', 'three', 'four', 'five']), target_extract)
+        self.assertEqual(test_center_csv.extract_test_center_row(['one', 'two', 'three', 'four', 'five']), target_extract)
 
     def test_csv_load_no_header(self):
         with self.assertRaises(Exception):
