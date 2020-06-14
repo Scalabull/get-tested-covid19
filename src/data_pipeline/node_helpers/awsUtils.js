a621d4e5fb0cdb9927666691006441f45dee28cf
Zach Boldyga <zboldyga@Gmail.com>
2020-06-14 14:44:48 -0400

    enabled AWS logging.

--- src/data_pipeline/node_helpers/awsUtils.js
+++ src/data_pipeline/node_helpers/awsUtils.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+AWS.config.logger = console;
 
 async function loadNewDiffFromS3(diffS3Key){
     const s3 = new AWS.S3();
@@ -7,7 +8,7 @@ async function loadNewDiffFromS3(diffS3Key){
         Bucket: 'staging-gtc-data-batches',
         Key: 'unver-staged-jobs/' + diffS3Key
     }
-    
+
     console.log('Getting diff with s3 params: ', JSON.stringify(params))
     const diff = await s3.getObject(params).promise();
     return diff;
